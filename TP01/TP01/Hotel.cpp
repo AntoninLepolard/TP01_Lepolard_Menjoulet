b93d7bc14c5540cce740fdfe2f250fd90dac8950
Aouh <lmenjoulet@gmail.com>
2020-11-13 11:04:25 +0100

    implementation hotel

--- TP01/TP01/Hotel.cpp
+++ TP01/TP01/Hotel.cpp
@@ -1,1 +1,24 @@
 #include "Hotel.h"
+
+Hotel::Hotel::Hotel(string id, string nom, string ville, int nbChambres) {
+    _id = id;
+    _nom = nom;
+    _ville = ville;
+    _chambres.resize(nbChambres);    
+}
+
+string Hotel::getId() {
+    return _id;
+}   
+
+string Hotel::getNom() {
+    return _nom;    
+}
+
+string Hotel::getVille() {
+    return _ville;    
+}
+
+vector<Chambre> Hotel::getChambres() {
+    return _chambres;    
+}
--- TP01/TP01/Hotel.h
+++ TP01/TP01/Hotel.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Chambre.h"
+
+using namespace std;
+
 class Hotel
 {
+    private:
+        string _id;
+        string _nom;
+        string _ville;
+        vector<Chambre> _chambres;
+    public:
+        Hotel(string id, string nom, string ville, int nbChambres);
+        string getId();
+        string getNom();
+        string getVille();
+        vector<Chambre> getChambres();
 };
 
