af88d253513977e1cbdec5ac202efb29f9fc0728
Aouh <lmenjoulet@gmail.com>
2020-11-13 10:34:42 +0100

    implementation client

--- TP01/TP01/Client.cpp
+++ TP01/TP01/Client.cpp
@@ -1,1 +1,44 @@
 #include "Client.h"
+#include <string>
+
+using namespace std;
+
+
+Client::Client(int id, string nom, string prenom, int nbReservations){
+    _id = id;
+    _nom = nom;
+    _prenom = prenom;
+    _nbReservations = nbReservations;
+}
+
+int Client::getId(){
+    return _id;
+}
+
+string Client::getNom(){
+    return _nom;
+}
+string Client::getPrenom(){
+    return _prenom;
+}
+int Client::getNbReservations(){
+    return _nbReservations;
+}
+
+
+
+void Client::setId(int id) {
+    _id = id;
+}
+
+void Client::setNom(string nom) {
+    _nom = nom;
+}
+
+void Client::Client::setPrenom(string prenom) {
+    _prenom = prenom;    
+}
+
+void Client::Client::setNbReservations(int nbReservations) {
+    _nbReservations = nbReservations;
+}
--- TP01/TP01/Client.h
+++ TP01/TP01/Client.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+
+using namespace std;
 class Client
 {
+    private:
+        int _id;
+        string _nom;
+        string _prenom;
+        int _nbReservations;
+    
+    public:
+        Client(int id, string nom, string prenom, int nbReservations);
+
+        //getters
+        int getId();
+        string getNom();
+        string getPrenom();
+        int getNbReservations();
+
+        //setters
+        void setId(int id);
+        void setNom(string nom);
+        void setPrenom(string prenom);
+        void setNbReservations(int nbReservations);
 };
 
